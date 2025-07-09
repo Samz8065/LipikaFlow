de9d1c4ada9ccc7c196866644a9f7ebda21a24f5
Samz8065 <samarthk0209@gmail.com>
2025-07-09 13:16:56 +0530

    made changes to editor and added multiple features

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,14 @@
 
 import React from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 import StarterKit from "@tiptap/starter-kit";
 
 export const Editor = () => {
@@ -13,7 +21,19 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      Image,
+      ImageResize,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+    ],
     content: "<p>Hello world!</p>",
   });
   return (
